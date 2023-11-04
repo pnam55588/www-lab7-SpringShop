d21b70e1d0f8722cee14879a875cc4d54c16bc09
pnam55588 <pnam55588@gmail.com>
2023-11-04 13:18:37 +0700

    init

---
+++ src/main/java/fit/iuh/lab7/Lab7Application.java
@@ -0,0 +1,13 @@
+package fit.iuh.lab7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7Application.class, args);
+    }
+
+}
